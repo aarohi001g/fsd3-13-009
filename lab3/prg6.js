c61fafa00d42d2fddad96de496f2ad8871872078
aarohi001g <aarohi.25b01010076@abes.ac.in>
2026-09-16 12:04:14 +0530

    add post request with body

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,27 +1,40 @@
-import http from 'http'
+import http from "http";
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/' && req.method === 'GET') {
-        res.statusCode = 200;
-        res.end("Get request is successful");
-    } else if (req.url === '/' && req.method === 'POST') {
-        res.statusCode = 200;
-        res.end("Post request is successful");
-    } else if (req.url === '/' && req.method === 'PUT') {
-        res.statusCode = 200;
-        res.end("PUT request is successful");
-    } else if (req.url === '/' && req.method === 'DELETE') {
-        res.statusCode = 200;
-        res.end("Delete request is successful");
-    }
+  if(req.url === '/' && req.method === 'GET') {
+    res.statusCode = 200;
+    res.end("Get Request"); 
+  }
+   else if(req.url === '/' && req.method === 'POST') {
+    // console.log("Request:",req);
+    let body = ''
+    req.on('data',(chunk)=>{
+      body += chunk
+    })
+    req.on("end",()=>{
+      const product = JSON.parse(body);
+      console.log("received products:", product);
+      res.statusCode = 201;
+   res.end(JSON.stringify({msg:'product added',product}));
+   });
 
-    else {
-        res.statusCode = 404;
-        res.end("Page not found");
-    }
-
-})
-
-server.listen(3000, () => console.log("prg6 is running on port 3000..."))
+  }
 
 
+  else if(req.url === '/' && req.method === 'PUT') {
+    res.statusCode = 200;
+    res.end("Put Request"); 
+  }
+  
+  else if(req.url === '/' && req.method === 'DELETE') {
+    res.statusCode = 200;
+    res.end("Delete Request"); 
+  }
+  else {
+    res.statusCode = 404;
+    res.end('request not found')
+  }
+
+});
+
+server.listen(3000, () => console.log("prg6 is running"));
