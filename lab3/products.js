3882ac95149d9411747bb9fcbe936dfcfefd1a11
aarohi001g <aarohi.25b01010076@abes.ac.in>
2026-09-22 14:28:55 +0530

    getaddproduct updated

--- lab3/products.js
+++ lab3/products.js
@@ -1,19 +1,16 @@
-const products=[
-    {id:1, name:'marker', qty:10, price:100},
-    {id:2, name:'pen', qty:20, price:200},
-    {id:3, name:'pencil', qty:30, price:300}
+const products = [
+    {"id":1,"name":'marker', "qty":100,"price":15},
+    {"id":2,"name":'pen', "qty":50,"price":20},
 ]
+let nextId = 3;
 
-let nextID= 3;
+export const getAllProducts = () => {
+    return products;
+}
 
-export const getAllProducts=()=>{
- return products;
-};
-
-export const addProduct=(item)=>{
-
-    item.id=nextId;
-     nextID++;
-    products.push(item);
-    return item;
+export const addProduct = (item) => {
+      item.id = nextId;
+      nextId++;
+      products.push(item);
+      return item;
 };
